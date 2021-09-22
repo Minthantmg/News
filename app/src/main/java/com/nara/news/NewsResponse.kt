f06f25a97faa224025caa807427f95015f1dbc70
lenovo <minthant180@gmail.com>
2021-09-23 01:34:04 +0630

    created package for data models

--- app/src/main/java/com/nara/news/NewsResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.nara.news
-
-data class NewsResponse(
-    val articles: List<Article>,
-    val status: String,
-    val totalResults: Int
-)
--- app/src/main/java/com/nara/news/model/Source.kt
+++ app/src/main/java/com/nara/news/model/Source.kt
@@ -1,4 +1,4 @@
-package com.nara.news
+package com.nara.news.model
 
 data class Source(
     val id: String,
