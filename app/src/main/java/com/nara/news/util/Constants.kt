a947f15837f8c0ff58edea7e07cd85d4dfb13d4a
lenovo <minthant180@gmail.com>
2021-09-22 22:43:21 +0630

    added Constants

---
+++ app/src/main/java/com/nara/news/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.nara.news.util
+
+class Constants {
+
+    companion object{
+        const val API_KEY = "686b43c2b223431994b45b75dcd20d2b"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
