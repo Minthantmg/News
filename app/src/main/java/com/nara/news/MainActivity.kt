290ddeb460bb2a8ce7f819c43d34500e7638aff8
lenovo <minthant180@gmail.com>
2021-09-25 13:57:45 +0630

    add viewmodel

--- app/src/main/java/com/nara/news/MainActivity.kt
+++ app/src/main/java/com/nara/news/MainActivity.kt
@@ -2,15 +2,27 @@ package com.nara.news
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.nara.news.Repository.NewsRepository
+import com.nara.news.db.ArticleDatabase
+import com.nara.news.ui.NewsViewModel
+import com.nara.news.ui.NewsViewModelProviderFactory
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel : NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this,viewModelProviderFactory).get(NewsViewModel::class.java)
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
