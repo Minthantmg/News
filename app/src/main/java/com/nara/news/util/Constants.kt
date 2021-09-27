38250c4f03c0663519576cc0b92ad82cd9d38c34
lenovo <minthant180@gmail.com>
2021-09-28 00:02:36 +0630

    added new constant for search bar

--- app/src/main/java/com/nara/news/util/Constants.kt
+++ app/src/main/java/com/nara/news/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
     companion object{
         const val API_KEY = "686b43c2b223431994b45b75dcd20d2b"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY =500L
     }
 }
